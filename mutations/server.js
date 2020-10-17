903360f55c558b55068a665c9d5e5a3757badd70
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-10-17 00:06:44 -0300

    Updates schema for multi messages
    Adds the possibility of creating new messages and updating old ones

--- mutations/server.js
+++ mutations/server.js
@@ -4,9 +4,25 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-type Mutation { setMessage(message: String): String }
+input MessageInput {
+  content: String
+  author: String
+}
 
-type Query { getMessage: String }
+type Message {
+  id: ID!
+  content: String
+  author: String
+}
+
+type Query {
+  getMessage(id: ID!): Message
+}
+
+type Mutation {
+  createMessage(input: MessageInput): Message
+  updateMessage(id: ID!, input: MessageInput): Message
+}
 `);
 
 // This class implements the RandomDie GraphQL type
